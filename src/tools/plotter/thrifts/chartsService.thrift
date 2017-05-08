b96c4d00f0b98acaa07c6a49830bdde6096f4a16
Francesco Romano <francesco.romano.1987@gmail.com>
2017-05-08 11:09:34 +0200

    Added also real time plots

--- src/tools/plotter/thrifts/chartsService.thrift
+++ src/tools/plotter/thrifts/chartsService.thrift
@@ -18,13 +18,36 @@ struct Chart {
     4: list<Series> series;
 }
 
+struct AxisInfo {
+    1: string type = "auto";
+    2: double minimum = 0;
+    3: double maximum = 30;
+}
+
+struct RealTimeChartRequest {
+    1: AxisInfo xAxis;
+    2: AxisInfo yAxis;
+}
+
+struct RealTimeChartResponse {
+    1: string id;
+    2: string streamingPortName;
+}
+
 struct ChartSimpleInfo {
     1: string id;
     2: string title;
 }
 
+struct RealTimeStreamData {
+    1: list<Series> newPoints;
+}
+
+
 service ChartsService {
     string plotBatchChart(1: Chart chart);
+    RealTimeChartResponse plotRealTimeChart(1: Chart chart, 2: RealTimeChartRequest chartInfo);
+
     oneway void closeAllCharts();
     oneway void closeChart(1: string chartID);
 
